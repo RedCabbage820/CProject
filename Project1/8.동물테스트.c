b9f35f53e3b512426dfc06c0123a0a8b3e13bc86
RedCabbage820 <seouji20@gmail.com>
2025-02-16 09:51:48 +0900

    2025-2-16
    re

---
+++ Project1/8.동물테스트.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+
+
+
+int main()
+{
+	system("title 숲속 동물 테스트");
+	printf("잠을 깼다.\n너는 니 몸이 사람이 아님을 느꼈다.\n\n숲속의 동물이 된 너는\n뭐 하고 있니?\n ");
+	int select;
+	printf("1.숲에 감히 입장하기");
+	scanf("%d", &select);
+
+	return 0;
+}
