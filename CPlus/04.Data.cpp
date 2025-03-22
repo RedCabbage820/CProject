220a47c4067f9d07e64ecb1599eb7e0e1196fbd3
RedCabbage820 <wowannong820@naver.com>
2025-03-22 11:54:53 +0900

    Yeeeeeeeee
    Yaaaaaaaaaa

--- CPlus/04.Data.cpp
+++ CPlus/04.Data.cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-using namespace std;
-
-
-
-int main()
-{
-	int a = 1;
-	float f = 1.5f; //소수점 6자리까지
-	double d = 1.5; //소수점 15자리까지
-	bool isDead = false; //True,False 개념이 C언어에는 없지만 C++에서 생김
-	string name = "초록매실";
-
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//
+//
+//int main()
+//{
+//	int a = 1;
+//	float f = 1.5f; //소수점 6자리까지
+//	double d = 1.5; //소수점 15자리까지
+//	bool isDead = false; //True,False 개념이 C언어에는 없지만 C++에서 생김
+//	string name = "초록매실";
+//	
+//	return 0;
+//}
